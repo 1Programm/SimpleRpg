8c58eb13135098f2683da26dbedaf24268bacce1
Genesis <mr_felix_becker@web.de>
2021-10-15 17:48:47 +0200

    Start of Resource Loading 0.2

--- src/main/java/com/progen/games/simlperpg/resources/IResourceHandler.java
+++ src/main/java/com/progen/games/simlperpg/resources/IResourceHandler.java
@@ -2,5 +2,7 @@ package com.progen.games.simlperpg.resources;
 
 public interface IResourceHandler {
 
+    void loadMap(String absPath);
     Object getResource(String name);
+    String rootPath();
 }
--- src/main/java/com/progen/games/simlperpg/resources/ResourcesLoader.java
+++ src/main/java/com/progen/games/simlperpg/resources/ResourcesLoader.java
@@ -2,16 +2,26 @@ package com.progen.games.simlperpg.resources;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResourcesLoader implements IResourceHandler{
+
+    private final String rootPath;
+    private final Map<String, Object> map = new HashMap<>();
+
+    public ResourcesLoader(String rootPath) {
+        this.rootPath = rootPath;
+    }
 
     private BufferedImage loadImage(String absPath) { //TODO: Load Error Image on Error
         BufferedImage img = null;
         try {
             img = ImageIO.read(new File(absPath));
-        } catch (IOException e) {       //TODO: Own Exception
+        } catch (IOException e) {                     //TODO: Own Exception
             e.printStackTrace();
         }
         return img;
@@ -19,7 +29,18 @@ public class ResourcesLoader implements IResourceHandler{
 
 
     @Override
+    public void loadMap(String absPath) {
+//        BufferedReader reader = new BufferedReader();
+//        ResourcesLoader.class.getResource()
+    }
+
+    @Override
     public Object getResource(String name) {
         return null;
+    }
+
+    @Override
+    public String rootPath() {
+        return rootPath;
     }
 }
